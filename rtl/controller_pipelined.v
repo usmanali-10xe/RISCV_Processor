a3740ad024b39930e180bab5373f9b14107b113b
usman ali <usman.ali@10xengineers.ai>
2021-12-03 11:15:15 +0500

    modified pipelining by adding forwarding muxers and hazard detection stall signal

--- rtl/controller_pipelined.v
+++ rtl/controller_pipelined.v
@@ -9,30 +9,32 @@ module controller_pipelined #(parameter AWIDTH=32, DWIDTH=32 )
 	output		[2:0]	ImmSel,
 	output  		RegWEn,
 	output  		BrUn,
-	output  		ASel,
-	output  		BSel,
+	output  		ASel, BSel,
+	output  	[1:0]	AfSel, BfSel,
 	output		[3:0]	ALUSel,
 	output			MemRW,
 	output		[1:0]	WBSel,
+	output			stall,
+	output			flush,
 	output		[2:0]	Size
 );
 	// instruction types based on opcode
-	localparam	rtype1 = 5'b01100, rtype2 = 5'b01110, 
-			itype1 = 5'b00000, itype2 = 5'b00011,
-			itype3 = 5'b00100, itype4 = 5'b00110,
-			itype5 = 5'b11001, itype6 = 5'b11100,
-			stype  = 5'b01000, sbtype = 5'b11000,
-			utype1 = 5'b00101, utype2 = 5'b01101,
-			ujtype = 5'b11011;
+	localparam	rtype1 = 7'b0110011, rtype2 = 7'b0111011, 
+			itype1 = 7'b0000011, itype2 = 7'b0001111,
+			itype3 = 7'b0010011, itype4 = 7'b0011011,
+			itype5 = 7'b1100111, itype6 = 7'b1110011,
+			stype  = 7'b0100011, sbtype = 7'b1100011,
+			utype1 = 7'b0010111, utype2 = 7'b0110111,
+			ujtype = 7'b1101111;
 			
-	wire [4:0] opcode_x,opcode_m,opcode_w;
+	wire [6:0] opcode_x,opcode_m,opcode_w;
 	wire [2:0] func3_x,func3_m,func3_w;
 	wire BrTrue;
-	assign opcode_x = inst_x [6:2];
+	assign opcode_x = inst_x [6:0];
 	assign func3_x  = inst_x [14:12];
-	assign opcode_m = inst_m [6:2];
+	assign opcode_m = inst_m [6:0];
 	assign func3_m  = inst_m [14:12];
-	assign opcode_w = inst_w [6:2];
+	assign opcode_w = inst_w [6:0];
 	assign func3_w  = inst_w [14:12];
 	
 	//assign func7[5] = inst [30];
@@ -48,8 +50,7 @@ module controller_pipelined #(parameter AWIDTH=32, DWIDTH=32 )
 			: (opcode_x==itype3)?			{1'b0,func3_x}
 			:					4'b0;
 	assign ASel 	= (opcode_x==sbtype||opcode_x==utype1||opcode_x==ujtype);
-	assign BSel	= !(opcode_x==rtype1||opcode_x==rtype2);		
-				
+	assign BSel	= !(opcode_x==rtype1||opcode_x==rtype2);				
 	assign ImmSel 	= (opcode_x==stype)?			3'b001
 			: (opcode_x==sbtype)?			3'b010
 			: (opcode_x==utype1||opcode_x==utype2)? 3'b011
@@ -68,6 +69,26 @@ module controller_pipelined #(parameter AWIDTH=32, DWIDTH=32 )
 	assign RegWEn	= !(opcode_w==sbtype||opcode_w==stype);
 	
 	//////////////// depends on branch//////
-	assign PCSel	= (opcode_x==sbtype)? BrTrue : opcode_x[4]; 
+	assign PCSel	= (opcode_x==sbtype)? BrTrue : opcode_x[6]; 
+
+	//////////////// Forwarding muxer selection//////
+
+	//wire x_have_rs1 = !(opcode_x==utype1||opcode_x==utype2||opcode_x==ujtype)&&!(opcode_x==0);
+	//wire x_have_rs2 = (opcode_x==rtype1||opcode_x==rtype2||opcode_x==stype||opcode_x==sbtype)&&!(opcode_x==0);
+	wire w_have_rd,m_have_rd; 
+	assign m_have_rd  = !(opcode_m==sbtype||opcode_m==stype)&&!(&inst_m[11:7]);
+	assign w_have_rd  = !(opcode_w==sbtype||opcode_w==stype)&&!(&inst_w[11:7]);
 	
+	assign AfSel 	= (m_have_rd&(inst_x[19:15]==inst_m[11:7]))?	2'b01 // rs1_x=rd_m;
+			: (w_have_rd&(inst_x[19:15]==inst_w[11:7]))?	2'b10 // rs1_x=rd_w;
+			:						2'b00; 
+	assign BfSel 	= (m_have_rd&(inst_x[24:20]==inst_m[11:7]))?	2'b01
+			: (w_have_rd&(inst_x[24:20]==inst_w[11:7]))?	2'b10
+			:						2'b00;
+	//////////////// stalling signal for load type//////
+	assign stall 	= m_have_rd&(inst_x[19:15]==inst_m[11:7]||inst_x[24:20]==inst_m[11:7])&(opcode_m==itype1); // rs1_x=rd_m; 
+	
+	//////////////// flushing for taken branch//////
+	assign flush 	= BrTrue&(opcode_m==sbtype); // rs1_x=rd_m; 
+
 endmodule 
--- rtl/top/RISCV_Processor_pipelined.v
+++ rtl/top/RISCV_Processor_pipelined.v
@@ -7,8 +7,8 @@ module RISCV_Processor_pipelined #(parameter DWIDTH=32, AWIDTH=32, IWIDTH=32, RA
 	wire [DWIDTH-1:0] pc_out_new, pc_out_m_new;				// updated PC
 	wire [IWIDTH-1:0] inst, inst_d, inst_x, inst_m, inst_w;			// data signals
 	
-	wire [DWIDTH-1:0] DataA, DataA_x;					// DataA signal
-	wire [DWIDTH-1:0] DataB, DataB_x, DataB_m;				// DataB signal
+	wire [DWIDTH-1:0] DataA, DataA_x, DataA_fx;					// DataA signal
+	wire [DWIDTH-1:0] DataB, DataB_x, DataB_fx, DataB_m;				// DataB signal
 	wire [DWIDTH-1:0] imm_x, imm_m, imm_w;					// immediate
 
 	wire [DWIDTH-1:0] in_a, in_b; 						// alu inputs(A's, B's mux out)
@@ -23,11 +23,13 @@ module RISCV_Processor_pipelined #(parameter DWIDTH=32, AWIDTH=32, IWIDTH=32, RA
 	wire MemRW;								// RW DMEM selection
 	wire [2:0]  Size;							// data size to be RW	
 	wire PCSel;								// PC Selection for branch
-	wire BSel, ASel;							// A and B data selection
+	wire BSel, ASel;							// A d B data selection
+	wire [1:0] BfSel, AfSel;						// A and B data forward selection
 	wire BrUn, BrEq, BrLT;							// BrComp in out signals
 	wire RegWEn;								// Reg write enable
+	wire stall;
 // PC
-	register pc(.clk(clk), .rst(rst), .load(1'b1), .data_in(pc_in), .data_out(pc_out));
+	register pc(.clk(clk), .rst(rst), .load(!stall), .data_in(pc_in), .data_out(pc_out));
 // next PC
 	assign pc_out_new = pc_out + 32'd4;
 // PCSel_mux
@@ -35,28 +37,30 @@ module RISCV_Processor_pipelined #(parameter DWIDTH=32, AWIDTH=32, IWIDTH=32, RA
 // IMEM
 	IMEM im(.clk(clk), .ILoad(1'b0), .IAddr(pc_out), .inst(inst), .instW(32'b0));
 //*********ID register*********//
-	register ID_inst(.clk(clk), .rst(rst), .load(1'b1), .data_in(inst), .data_out(inst_d));
-	register ID_pc(.clk(clk), .rst(rst), .load(1'b1), .data_in(pc_out), .data_out(pc_out_d));
+	register ID_inst(.clk(clk), .rst(rst), .load(!stall), .data_in(inst), .data_out(inst_d));
+	register ID_pc(.clk(clk), .rst(rst), .load(!stall), .data_in(pc_out), .data_out(pc_out_d));
 // REGFILE
 	RegFile rfile(.clk(clk) , .RegWEn(RegWEn), .AddrA(inst_d[19:15]), .AddrB(inst_d[24:20]), .AddrD(inst_w[11:7]), .DataD(wb) , .DataA (DataA), .DataB (DataB));
 //*********IE register*********//
-	register IE_DataA(.clk(clk), .rst(rst), .load(1'b1), .data_in(DataA), .data_out(DataA_x));
-	register IE_DataB(.clk(clk), .rst(rst), .load(1'b1), .data_in(DataB), .data_out(DataB_x));
-	register IE_pc(.clk(clk), .rst(rst), .load(1'b1), .data_in(pc_out_d), .data_out(pc_out_x));
-	register IE_inst(.clk(clk), .rst(rst), .load(1'b1), .data_in(inst_d), .data_out(inst_x));
+	register IE_DataA(.clk(clk), .rst(rst), .load(!stall), .data_in(DataA), .data_out(DataA_x));
+	register IE_DataB(.clk(clk), .rst(rst), .load(!stall), .data_in(DataB), .data_out(DataB_x));
+	register IE_pc(.clk(clk), .rst(rst), .load(!stall), .data_in(pc_out_d), .data_out(pc_out_x));
+	register IE_inst(.clk(clk), .rst(rst), .load(!stall), .data_in(inst_d), .data_out(inst_x));
 // BranchComp
 	BranchComp br_cmp(.BrUn(BrUn), .BrA(DataA_x), .BrB(DataB_x), .BrEq(BrEq), .BrLT(BrLT));
 // ImmGen
 	ImmGen imgen(.inst(inst_x[IWIDTH-1:7]), .ImmSel(ImmSel), .Imm(imm_x));
 // BSel_mux
-	Mux2 b_mux (.sel(BSel), .in0(DataB_x), .in1(imm_x), .mux_out(in_b));
+	Mux4 b_forward_mux(.sel(BfSel), .in0(DataB_x), .in1(alu_out_m), .in2(wb), .in3(32'b0), .mux_out(DataB_fx));
+	Mux2 b_mux (.sel(BSel), .in0(DataB_fx), .in1(imm_x), .mux_out(in_b));
 // ASel_mux
-	Mux2 a_mux (.sel(ASel), .in0(DataA_x), .in1(pc_out_x), .mux_out(in_a));
+	Mux4 a_forward_mux(.sel(AfSel), .in0(DataA_x), .in1(alu_out_m), .in2(wb), .in3(32'b0), .mux_out(DataA_fx));
+	Mux2 a_mux (.sel(ASel), .in0(DataA_fx), .in1(pc_out_x), .mux_out(in_a));
 // ALU
 	ALU alu(.in_a(in_a), .in_b(in_b), .ALUSel(ALUSel), .alu_out(alu_out));
 //*********MEM register*********//
 	register MEM_alu_out(.clk(clk), .rst(rst), .load(1'b1), .data_in(alu_out), .data_out(alu_out_m));
-	register MEM_DataB(.clk(clk), .rst(rst), .load(1'b1), .data_in(DataB_x), .data_out(DataB_m));
+	register MEM_DataB(.clk(clk), .rst(rst), .load(1'b1), .data_in(DataB_fx), .data_out(DataB_m));
 	register MEM_pc(.clk(clk), .rst(rst), .load(1'b1), .data_in(pc_out_x), .data_out(pc_out_m));
 	register MEM_inst(.clk(clk), .rst(rst), .load(1'b1), .data_in(inst_x), .data_out(inst_m));
 	register MEM_imm(.clk(clk), .rst(rst), .load(1'b1), .data_in(imm_x), .data_out(imm_m));
@@ -74,7 +78,7 @@ module RISCV_Processor_pipelined #(parameter DWIDTH=32, AWIDTH=32, IWIDTH=32, RA
 	Mux4 wb_mux(.sel(WBSel), .in0(mem_w), .in1(alu_out_w), .in2(pc_out_w), .in3(imm_w), .mux_out(wb));
 // Controller
 	controller_pipelined control (.BrEq(BrEq), .BrLT(BrLT), .inst_x(inst_x), .inst_m(inst_m),.inst_w(inst_w),  .PCSel(PCSel),  .ImmSel(ImmSel),
-		                 .RegWEn(RegWEn),  .BrUn(BrUn), .ASel(ASel), .BSel(BSel), .ALUSel(ALUSel),
-		                 .MemRW(MemRW), .WBSel(WBSel), .Size(Size));
+		                 .RegWEn(RegWEn),  .BrUn(BrUn), .ASel(ASel), .BSel(BSel), .AfSel(AfSel), .BfSel(BfSel), .ALUSel(ALUSel),
+		                 .MemRW(MemRW), .WBSel(WBSel), .Size(Size), .stall(stall), .flush(flush));
 
 endmodule
