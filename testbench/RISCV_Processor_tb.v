82f92593cebb5cfeb8e34b4cf4b9248a3783f4ff
Usman Ali <usman.ali@10xengineers.ai>
2022-01-07 20:06:55 +0500

    adding some test cases

--- testbench/RISCV_Processor_tb.v
+++ testbench/RISCV_Processor_tb.v
@@ -12,26 +12,33 @@ reg clk,rst;
     end
   endtask
 
-RISCV_Processor_pipelined riscv_inst(clk,rst);
+RISCV_Processor_pipelined_withBP riscv_inst(clk,rst);
 
 initial begin
 // checking total number of even and odd numbers in a series by saving it in an array of words(4bytes)
 	// initial number of seires 3x+1, must be saved in x28 register
-	riscv_inst.rfile.register[28] = 32'd3;  
+	//riscv_inst.rfile.register[28] = 32'd3;  
 	// total numbers to be stored in array, must be saved in x21 register
-	riscv_inst.rfile.register[21] = 32'd10;
-   	$readmemh("even_odd_from_stored_series.text", riscv_inst.im.Instruction) ;
+	//riscv_inst.rfile.register[21] = 32'd10;
+   	//$readmemh("even_odd_from_stored_series.text", riscv_inst.im.Instruction) ;
 	// total even numbers are saved in x8 register and odd numebrs are saved in x9 register
 	// you can also check saved array in dmemory
         
 // to check that all instructions are workin uncomment this line
-	//$readmemh("RISCV_test1.text", riscv_inst.im.Instruction) ;
+	//$readmemh("A_RISCV_test.text", riscv_inst.im.Instruction) ;
 
 // to check nth fibonacci number 
     	//$readmemh("fibonacci_test.text", riscv_inst.im.Instruction) ;
-	reset;
-	clock(200);
 
-$finish;
+// to check the binomial series number
+	//$readmemh("Binomial.text", riscv_inst.im.Instruction);
+
+// to check the factorial of 5
+	//$readmemh("factorial.text", riscv_inst.im.Instruction);
+// to check LCM
+	$readmemh("LCM.text", riscv_inst.im.Instruction);
+	reset;
+	clock(500);
+
 end
 endmodule 
