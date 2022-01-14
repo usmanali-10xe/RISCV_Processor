1570b56daf89118b49c81a74c3e1c9e42d56417c
Usman Ali <usman.ali@10xengineers.ai>
2022-01-14 13:13:43 +0500

    Add comments

--- testbench/RISCV_Processor_tb.v
+++ testbench/RISCV_Processor_tb.v
@@ -17,9 +17,9 @@ RISCV_Processor_pipelined_withBP riscv_inst(clk,rst);
 initial begin
 // checking total number of even and odd numbers in a series by saving it in an array of words(4bytes)
 	// initial number of seires 3x+1, must be saved in x28 register
-	//riscv_inst.rfile.register[28] = 32'd3;  
+	//riscv_inst.rfile.register[28] = 32'd3;  //to change its value uncomment this and comment the hex file given for this programm
 	// total numbers to be stored in array, must be saved in x21 register
-	//riscv_inst.rfile.register[21] = 32'd10;
+	//riscv_inst.rfile.register[21] = 32'd10; //to change its value uncomment this and comment the hex file given for this programm
    	//$readmemh("even_odd_from_stored_series.text", riscv_inst.im.Instruction) ;
 	// total even numbers are saved in x8 register and odd numebrs are saved in x9 register
 	// you can also check saved array in dmemory
